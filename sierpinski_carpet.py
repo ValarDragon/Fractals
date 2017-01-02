84f50e45dc347dbda1a2cd8964be168048297626
ValarDragon <dojha12@gmail.com>
2017-01-02 02:14:27 -0600

    delete dummy files. Completed creating polygons for initial phase, making rotations look normal

--- sierpinski_carpet.py
+++ sierpinski_carpet.py
@@ -9,10 +9,11 @@ import math
 
 def main(argv):
 
-    imgname = "carpet_" + strftime("%m-%d_%H:%M", gmtime())+".jpg"
+
     canvas_size = 2000
-    sides = 3
+    sides = 4
     sidelength = 600
+    imgname = "carpet_" + str(sides) + "_" + strftime("%m-%d_%H:%M", gmtime())+".jpg"
     bgcolor = "black"
     fillcolor = ["#33DDDD","#55BBCC","#66AACC","#7788BB","#8977AC","#9F66BC","#AA66AA","#CC99CC","#CCDDDD"]
     try:
@@ -33,14 +34,23 @@ def main(argv):
 
     innertheta = 2*math.pi / sides
     if(sides % 2 == 0):
-        print('tbc')
-    else:
         lengthfromcenter = l / (math.sqrt(2-2*math.cos(innertheta)))
         points = []
-        theta = 0
+        #theta = 90 degree so that the first point is directly upward, instead of directly right
+        theta = math.pi/2 - (innertheta/2)
         for i in range(sides):
             points.append( (c + lengthfromcenter*math.cos(theta), c - lengthfromcenter*math.sin(theta)) )
             theta += innertheta
+        draw.polygon(points, fillcolor[0])
+    else:
+        lengthfromcenter = l / (math.sqrt(2-2*math.cos(innertheta)))
+        points = []
+        #theta = 90 degree so that the first point is directly upward, instead of directly right
+        theta = math.pi/2
+        for i in range(sides):
+            points.append( (c + lengthfromcenter*math.cos(theta), c - lengthfromcenter*math.sin(theta)) )
+            theta += innertheta
+
 
         draw.polygon(points, fillcolor[0])
 
