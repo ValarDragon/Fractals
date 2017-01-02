1b0d7220017f4aea856289f3f6e69cbfff6a332c
ValarDragon <dojha12@gmail.com>
2017-01-02 13:15:13 -0600

    Core recursive polygon generator, needs the new sides rotation to be adjusted though.

--- sierpinski_carpet.py
+++ sierpinski_carpet.py
@@ -6,18 +6,26 @@ import sys, getopt
 from time import gmtime, strftime
 import math
 
-
 def main(argv):
 
-
-    canvas_size = 2000
-    sides = 4
+    global sides,fillcolor,iterations,scalingfactor,numonline,innertheta,canvas_size,innerlengthconstant
+    global rotSides
+    canvas_size = 3000
+    sides = 7
     sidelength = 600
-    imgname = "carpet_" + str(sides) + "_" + strftime("%m-%d_%H:%M", gmtime())+".jpg"
+    iterations = 5
+    scalingfactor = 1/3
+    numonline = 1
+    rotSides = True
+
+    #Not global, intentional.
+    m2cdist = 400
+
+    imgname = ""
     bgcolor = "black"
     fillcolor = ["#33DDDD","#55BBCC","#66AACC","#7788BB","#8977AC","#9F66BC","#AA66AA","#CC99CC","#CCDDDD"]
     try:
-        opts, args = getopt.getopt(argv,"o:c:",["output=","canvas="])
+        opts, args = getopt.getopt(argv,"s:o:c:",["sides=","output=","canvas="])
     except getopt.GetoptError:
         print('sierpinski_carpet.py -o <output image name> -c <canvas size in pixels>')
         sys.exit(2)
@@ -26,40 +34,69 @@ def main(argv):
             imgname = arg
         elif opt in ("-c", "--canvas"):
             canvas_size = int(arg)
+        elif opt in ("-s", "--sides"):
+            sides = int(arg)
+
+    imgname = "carpet_" + str(sides) + "_" + strftime("%m-%d_%H:%M", gmtime())+".jpg"
 
     img = Image.new("RGBA",(canvas_size,canvas_size),bgcolor)
     c = canvas_size/2 #center
-    l = sidelength
+    sidelength
     draw = ImageDraw.Draw(img)
 
     innertheta = 2*math.pi / sides
+    theta = 0
     if(sides % 2 == 0):
-        lengthfromcenter = l / (math.sqrt(2-2*math.cos(innertheta)))
-        points = []
-        #theta = 90 degree so that the first point is directly upward, instead of directly right
         theta = math.pi/2 - (innertheta/2)
-        for i in range(sides):
-            points.append( (c + lengthfromcenter*math.cos(theta), c - lengthfromcenter*math.sin(theta)) )
-            theta += innertheta
-        draw.polygon(points, fillcolor[0])
     else:
-        lengthfromcenter = l / (math.sqrt(2-2*math.cos(innertheta)))
-        points = []
-        #theta = 90 degree so that the first point is directly upward, instead of directly right
         theta = math.pi/2
-        for i in range(sides):
-            points.append( (c + lengthfromcenter*math.cos(theta), c - lengthfromcenter*math.sin(theta)) )
-            theta += innertheta
 
-
-        draw.polygon(points, fillcolor[0])
+    innerlengthconstant = math.sqrt(2-2*math.cos(innertheta))
+    lengthfromcenter = sidelength / innerlengthconstant
+    points = []
+    for i in range(sides):
+        points.append( (c + lengthfromcenter*math.cos(theta), c - lengthfromcenter*math.sin(theta)) )
+        theta += innertheta
+    draw.polygon(points, fillcolor[0])
+    print("center: " + str(c))
+    recurse(points,theta,sidelength*scalingfactor,(c,c),1,m2cdist,draw)
 
     if("/" not in imgname):
-        imgname = "output/carpet/carpet_"+imgname
+        imgname = "output/carpet/"+imgname
     img.save(imgname,"JPEG")
 
+def recurse(points,theta0point,newlength,center,curdepth,m2cdist,draw):
+    if(curdepth > iterations):
+        return
+    #loop through each side
+    for i in range(len(points)):
+        p1 = points[i]
+        p2 = points[(i+1)%sides]
+        midpoint = ((p1[0]+p2[0])/2,(p1[1]+p2[1])/2)
+        print("midpoint: " + str(midpoint))
+        #center to midpoint is the slope that is perpendicular to the side
+        #slope = (p2[1]-p1[1])/(p2[0]-p1[0])
+        #draw.line((center,midpoint),"red")
 
+        theta = math.atan2(midpoint[1]-center[1],midpoint[0]-center[0])
+        if(theta < 0):
+            theta += 2*math.pi
+        newcenter = (midpoint[0] + m2cdist*math.cos(theta), midpoint[1] + m2cdist*math.sin(theta))
 
+        lengthfromcenter = newlength / innerlengthconstant
+        newpoints = []
+        #if(rotSides):
+        #    if(sides % 2 == 0):
+        #        theta += math.pi/2 - (innertheta/2)
+            #else:
+            #    theta -= math.pi/2
+        print(points)
+        print(180*theta/math.pi)
+        for i in range(sides):
+            newpoints.append( (newcenter[0] + lengthfromcenter*math.cos(theta), newcenter[1] - lengthfromcenter*math.sin(theta)) )
+            theta += innertheta
+        draw.polygon(newpoints, fillcolor[curdepth])
+        recurse(newpoints,theta,newlength*scalingfactor,newcenter,curdepth+1,m2cdist*scalingfactor,draw)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
